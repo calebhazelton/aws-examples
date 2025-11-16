b44ca81ce0b0332b531256d79b25c580325a2bb6
Caleb <160376471+calebhazelton@users.noreply.github.com>
2025-11-16 23:23:17 +0000

    devcontainer Init

---
+++ devcontainer/install-aws-cli.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt-get update
+sudo apt-get install -y unzip curl
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
