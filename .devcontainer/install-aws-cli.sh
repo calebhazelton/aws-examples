c3c4fb28b1be32049083c7a6bd065227b6a69ec4
Caleb <160376471+calebhazelton@users.noreply.github.com>
2025-11-17 03:01:54 +0000

    configured secrets

--- .devcontainer/install-aws-cli.sh
+++ .devcontainer/install-aws-cli.sh
@@ -1,3 +1,9 @@
+cd /workspaces
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+cd $CODESPACE_VSCODE_FOLDER
+
+export $AWS_ACCESS_KEY
+export $AWS_SECRET_ACCESS_KEY
+export $AWS_REGION
