a6cf5c1854323f96fe44a99c5999c64290ba0fee
Caleb <160376471+calebhazelton@users.noreply.github.com>
2025-11-23 03:05:31 +0000

    removd manual env export

--- .devcontainer/install-aws-cli.sh
+++ .devcontainer/install-aws-cli.sh
@@ -3,7 +3,3 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip -o awscliv2.zip
 sudo ./aws/install
 cd $CODESPACE_VSCODE_FOLDER
-
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ENV
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_ENV
-export AWS_REGION=us-east-1
